abe53461a37669cee8e8a1f6f3f7f0d09101b9dc
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2025-08-04 11:14:08 +0530

    created server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();  
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+
+
+app.listen (3000, () => {
+  console.log('Server is running on port 3000');
+});
 
